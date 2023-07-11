7ce2249541a898de34998bc296ae1aacae214e8f
Mohit Kamdi <mohitkamdi317@gmail.com>
2023-07-11 13:25:02 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
         {/* Do not remove the main div */}
         <label>Enter Your Name:</label><br/>
         <input type="text" onChange={inputChangeHandler}/><br/>
-        <div>{name}</div>
+        <p>{name}</p>
     </div>
   )
 }
